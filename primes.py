33bd3f454baed65d54c1dc408b706da81179728b
vicmin03 <vicmin03@outlook.com>
2022-10-01 18:23:07 +0100

    Generate list of primes of given length

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(no):
+    if no == 2:
+        return True
+    for i in range (2, no):
+        if (no%i == 0):
+            return False
+    return True
+
 def primes(number_of_primes):
-    list = []
-    return list
+    primes_list = []
+    current = 2
+    while len(primes_list) < number_of_primes:
+        if (is_prime(current)):
+            primes_list.append(current)
+        current += 1
+    print(number_of_primes, primes_list)
+    return primes_list
